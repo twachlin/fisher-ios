7394eb4f18e300556ab1a44b8b01e76cccec4620
Tomas Wachlin <tomaswachlin@gmail.com>
2026-05-30 17:06:37 -0300

    Initial Commit

---
+++ fisher-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fisher-ios
+//
+//  Created by Tomas Wachlin on 30/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fisher-ios/fisher_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fisher_iosApp.swift
+//  fisher-ios
+//
+//  Created by Tomas Wachlin on 30/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct fisher_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
